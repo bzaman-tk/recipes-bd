3f11ef7c1c42646bfcb07532fa53160b494ff7b1
bzaman_tk <bzaman.tk@gmail.com>
2023-05-02 14:53:22 +0600

    2nd extra section added

--- src/components/Home/ExtraSectionTwo.jsx
+++ src/components/Home/ExtraSectionTwo.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ExtraSectionTwo = () => {
     return (
-        <div className='extra-two bg-gray-400 py-12 text-black'>
+        <div className='extra-two bg-gray-600 py-12 text-white'>
             <div className="container mx-auto">
                 <div className="section-headr text-center mb-9">
-                    <h2 className="section-title text-4xl font-bold m-0 mb-4">About Us</h2>
-                    <p className="text-lg m-0">We have the best Chef's of bangladesh who provide native cusine</p>
+                    <h2 className="section-title text-4xl font-bold m-0 mb-4"><span className='block text-base'>About</span><span className="font-bold normal-case mx-2"> Recipes <span className="text-green-700">B<span className="text-red-700">D</span></span></span></h2>
+                    <p className="text-lg m-0 max-w-full w-4/5 mx-auto">"Recipes BD" is a vibrant Bangladeshi restaurant that brings the flavors and traditions of Bangladesh to your plate. With an extensive menu of authentic Bangladeshi dishes, from spicy curries to savory biryanis, every dish is prepared with fresh ingredients and cooked to perfection. The warm and inviting ambiance, combined with the rich aromas and bold spices, creates a dining experience that will transport you to the bustling streets of Dhaka. Come and experience the taste of Bangladesh at "Recipes BD".</p>
                 </div>
             </div>
             <div className="about-wrap">
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from './Hero';
 import Chefs from './chefs';
 import ExtraSectionOne from './ExtraSectionOne';
+import ExtraSectionTwo from './ExtraSectionTwo';
 
 const Home = () => {
     return (
         <div className='home-wrap'>
             <Hero />
             <Chefs />
+            <ExtraSectionTwo />
             <ExtraSectionOne />
         </div>
     );
